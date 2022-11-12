ccdbc963eafd56d3c9bc0206cc0d89f8be5e5a48
EddyGiusepe <eddychirinos.unac@gmail.com>
2022-11-12 16:53:39 -0300

    Final the project

--- projeto_1_docker-compose/app.py
+++ projeto_1_docker-compose/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
 def get_hit_count():
-    '''Função que conta os incrementos'''
+    '''Função que conta as visualizações da página WEB'''
     retries = 5
     while True:
         try:
@@ -27,4 +27,10 @@ def get_hit_count():
 def hello():
     '''Printa na Interface.'''
     count = get_hit_count()
-    return f"Olá colegas da Vale {count}."
+    return f"Feliz de trabalahar com vocês! Vamos seguir com o estudo de NLP-Transformers.{count}."
+
+@app.route("/rota_eddy", methods=["GET"])
+def func():
+    '''Mais uma rota para testar'''
+    return '<h1>Está foi uma apresentação simples de <font color="blue">docker-compose.yml</font></h1>'
+
