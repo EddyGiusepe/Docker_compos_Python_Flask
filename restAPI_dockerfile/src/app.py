825e63891c1c23f33fa85b222100bf0f4dd12b98
EddyGiusepe <eddychirinos.unac@gmail.com>
2022-11-13 00:44:21 -0300

    Segundo projeto apenas Dockerfile

---
+++ restAPI_dockerfile/src/app.py
@@ -0,0 +1,23 @@
+'''
+Data Scientist.: Dr.Eddy Giusepe Chirinos Isidro
+'''
+from flask import Flask, jsonify
+from users import users
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def ping():
+    '''Rota inicial da minha aplicação'''
+    return jsonify({"response": "Olá colegas da UFES-Vale"})
+
+@app.route("/users")
+def usershandler():
+    '''Rota de Usuários.'''
+    return jsonify({"users": users})
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=4000, debug=True)
---
+++ restAPI_dockerfile/src/users.py
@@ -0,0 +1,8 @@
+'''
+Data Scientist.: Dr.Eddy Giusepe Chirinos Isidro
+'''
+users = [
+    {"name": "Deus"},
+    {"name": "Eddy"},
+    {"name": "Karina"}
+]
